507c43bfbccb93a3dca710b5e1fbb659306085c9
Amit Nayak <unikreate@zohomail.in>
2026-06-03 19:27:35 +0530

    a

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 
-export default function App() {
+// App.tsx
+export { default } from 'expo-router/entry';
   const ctx = require.context('./app');
   return <ExpoRoot context={ctx} />;
 }
